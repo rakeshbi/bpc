5a23c656331b2051d8367e9b3f0c99b4fb706b58
rakeshbi <92093051+rakeshbi@users.noreply.github.com>
2021-12-20 11:21:24 +0530

    added environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  storage: localStorage,
+  // baseUri: 'http://52.40.207.123/abhidemo/',
+  // baseUri: 'http://52.40.207.123/abhidemotest/',
+  baseUri: 'http://localhost/beta/api/',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,21 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  storage: sessionStorage,
+  // baseUri: 'http://52.40.207.123/abhidemotest/',
+  // baseUri: 'http://52.40.207.123/abhidemo/'
+  // baseUri: 'https://beta.pure.bi/api/',
+  baseUri: 'http://localhost/api/',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
